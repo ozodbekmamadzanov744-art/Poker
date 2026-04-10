675df17f993fb05d65d96610357ccff9831c760f
ozodbekmamadzanov744-art <ozodbekmamadzanov744@gmail.com>
2026-04-10 22:40:29 +0600

    lab 1

--- Poker/src/Main.java
+++ Poker/src/Main.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Какое сейчас время: ");
+        int currentTime = sc.nextInt();
+
+        if (currentTime >= 7 && currentTime <= 11){
+            System.out.println("Доброе утро ");
+        }else if (currentTime >= 11 && currentTime <= 15){
+            System.out.println("Добрый день ");
+        }else {
+            System.out.println("Добрый вечер ");
         }
     }
 }
